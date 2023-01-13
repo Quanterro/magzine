4480991fe1d516bf58ad06d746b592641e03f751
Szymon <abramskiszymon@gmail.com>
2023-01-13 21:12:06 +0100

    Initial commit

---
+++ src/main/java/com/app/magzine/MagzineApplication.java
@@ -0,0 +1,13 @@
+package com.app.magzine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MagzineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MagzineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/app/magzine/MagzineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.magzine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MagzineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
