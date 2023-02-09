cb2e02ebc93fa8e4a691bc34ea6911441722e86f
Quanterro <quanterroz@gmail.com>
2023-02-09 21:06:34 +0100

    Changed spring security configuration, it is possible to add new entries to the database

--- src/main/java/com/app/magzine/model/Microphone.java
+++ src/main/java/com/app/magzine/model/Microphone.java
@@ -1,6 +1,8 @@
 package com.app.magzine.model;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -12,6 +14,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class Microphone {
 	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String model;
 	private String brand;
