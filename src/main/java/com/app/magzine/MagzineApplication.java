347d80090df9e644ccc2dd02083a29ddb25c7026
Quanterro <quanterroz@gmail.com>
2023-02-09 19:53:43 +0100

    Added swagger

--- src/main/java/com/app/magzine/MagzineApplication.java
+++ src/main/java/com/app/magzine/MagzineApplication.java
@@ -4,8 +4,11 @@ package com.app.magzine;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+
+
 @SpringBootApplication
-public class MagzineApplication {
+class MagzineApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MagzineApplication.class, args);
